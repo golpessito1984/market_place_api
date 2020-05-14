56ac41300d16e5d3b80dda18761d5f98a4125f20
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2020-05-14 13:27:56 +0200

    User create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,20 @@ class Api::V1::UsersController < ApplicationController
     render json: @user, status: :ok
   end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 
   def set_user
     @user = User.find(params[:id])
--- spec/controllers/api/v1/users_controllers_spec.rb
+++ spec/controllers/api/v1/users_controllers_spec.rb
@@ -21,4 +21,35 @@ RSpec.describe Api::V1::UsersController do
       expect(hash_body["errors"]).to eq('Record not found')
     end
   end
+
+  describe 'User POST #create' do
+    before(:each) do
+      @user_params = {user: {email: 'david.ruizdelarosa@gmail.com',
+                             password: '222333'}}
+    end
+
+    it 'return a successfully new user' do
+      expect do
+        post :create, params: @user_params
+      end.to change(User, :count).by(1)
+
+      expect(response.status).to eq(201)
+      hash_body = JSON.parse(response.body)
+      expect(hash_body["email"]).to eq(@user_params[:user][:email])
+    end
+
+    it 'return a error message when params ara invalid' do
+      user_invalid_params = {user: {email: 'david.ruizdelarosa',
+                                    password: '222333'}}
+
+      expect do
+        post :create, params: user_invalid_params
+      end.to change(User, :count).by(0)
+
+      expect(response.status).to eq(422)
+      hash_body = JSON.parse(response.body)
+      expect(hash_body["email"]).to match_array(["is invalid"])
+    end
+
+  end
 end
